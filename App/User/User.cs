e3d45757c28967db81fdc59964cd9ed14769dca0
Linus Dehner <if21b132@technikum-wien.at>
2021-11-24 18:15:43 +0100

    Revert wrong commit message
    This reverts commit 22937018fdf2c9089f4a12727836130c0deb433d.

--- App/User/User.cs
+++ App/User/User.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using MonsterTradingCardsGame.App.Cards;
 
-namespace MonsterTradingCardsGame.App.User
+namespace MonsterTradingCardsGame.App
 {
     public class User
     {
