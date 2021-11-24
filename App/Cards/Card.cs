c81e96839c5b0c9a545c09521780e709ec247b3e
Linus Dehner <if21b132@technikum-wien.at>
2021-11-24 18:16:25 +0100

    added rule list

--- App/Cards/Card.cs
+++ App/Cards/Card.cs
@@ -1,16 +1,19 @@
-using System;
+using System.Collections.Generic;
+using MonsterTradingCardsGame.App.Rules;
 
-namespace MonsterTradingCardsGame.App
+namespace MonsterTradingCardsGame.App.Cards
 {
     public abstract class Card
     {
         public int Damage { get; set; }
         public readonly Modification Mod;
+        public readonly List<Rule> Rules;
 
         public Card(int damage, Modification mod)
         {
             Damage = damage;
             Mod = mod;
+            Rules = new List<Rule>();
         }
 
         public abstract string GetCardName();
