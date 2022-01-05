6deeb77f4687f5b629bf27b248df7e4059007eff
Linus Dehner <if21b132@technikum-wien.at>
2022-01-05 21:58:44 +0100

    added exceptions

---
+++ MonsterTradingCardsGameServer/DuplicateUserException.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace MonsterTradingCardsGameServer
+{
+    public class DuplicateUserException : Exception
+    {
+        public DuplicateUserException()
+        {
+        }
+
+        public DuplicateUserException(string message) : base(message)
+        {
+        }
+
+        public DuplicateUserException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
