060771795f30aefaf62fd1e15cc3a79f9e5c9d0d
Linus Dehner <if21b132@technikum-wien.at>
2022-01-10 23:02:23 +0100

    redisigned card layout

--- MonsterTradingCardsGameServer/Cards/Card.cs
+++ MonsterTradingCardsGameServer/Cards/Card.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using MonsterTradingCardsGameServer.Rules;
 
@@ -6,14 +7,16 @@ namespace MonsterTradingCardsGameServer.Cards
     public abstract class Card
     {
         public int Damage { get; set; }
-        public readonly Modification Mod;
-        public readonly List<Rule> Rules;
+        public Modification Mod { get; set; }
+        public List<Rule> Rules { get; set; }
+        public Guid Id { get; set; }
 
         public Card(int damage, Modification mod)
         {
             Damage = damage;
             Mod = mod;
             Rules = new List<Rule>();
+            Id = Guid.NewGuid();
         }
 
         public abstract string GetCardName();
