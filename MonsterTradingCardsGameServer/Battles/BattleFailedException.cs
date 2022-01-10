b2f64cb83ab5214b03f2e124ffbef3a5fbf3d6f9
Linus Dehner <if21b132@technikum-wien.at>
2022-01-10 23:00:44 +0100

    added exception in case battle fails

---
+++ MonsterTradingCardsGameServer/Battles/BattleFailedException.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace MonsterTradingCardsGameServer.Battles
+{
+    public class BattleFailedException: Exception
+    {
+        public BattleFailedException()
+        {
+        }
+
+        public BattleFailedException(string message) : base(message)
+        {
+        }
+
+        public BattleFailedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
